ee16575c4467ad1808129f2cab86271e98c539e6
AbeHaruhiko <caliconography@gmail.com>
2015-04-09 00:00:26 +0900

    [modify] 一件更新追加。 P81

--- src/main/java/demo/api/CustomerRestController.java
+++ src/main/java/demo/api/CustomerRestController.java
@@ -3,9 +3,12 @@ package demo.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import demo.domain.Customer;
@@ -30,5 +33,19 @@ public class CustomerRestController {
 	Customer getCustomer(@PathVariable Integer id) {
 		Customer customer = customerService.findOne(id);
 		return customer;
+	}
+
+	// 新規作成
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	Customer postCustomers(@RequestBody Customer customer) {
+		return customerService.create(customer);
+	}
+
+	// 一件更新
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+		customer.setId(id);
+		return customerService.update(customer);
 	}
 }
